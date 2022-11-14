754043c3a1af80bdfc3c0bda8fdd45ebde32921b
Furkan <furi544@gmail.com>
2022-11-14 21:32:07 +0300

    updated auth api

--- src/store/api/auth.js
+++ src/store/api/auth.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import Songs from 'song.json'
+// import Songs from 'song.json'
 import { API_URL } from 'utils/URL'
 
 // Define a service using a base URL and expected endpoints
@@ -7,18 +7,24 @@ export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }),
   endpoints: (builder) => ({
-    register: builder.query({
-      query: () => Songs.song + '-' + Songs.artist,
-      
+    register: builder.mutation({
+      query: (data) => ({
+        url: 'api/auth/local/register',
+        method: 'POST',
+        body: JSON.stringify(data),
+      }),
     }),
     login: builder.query({
-      query: () => Songs.song + '-' + Songs.artist,
+      // query: () => Songs.song + '-' + Songs.artist,
     }),
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetDataQuery } = authApi
+export const { useRegisterMutation } = authApi
