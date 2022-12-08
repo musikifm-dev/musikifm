e880518fda5f54e8b6361d53699120ae8968db52
Furkan <furi544@gmail.com>
2022-12-08 18:15:59 +0300

    deeted unnessesaary comps

--- src/sections/sliders/components/hero-slider/index.jsx
+++ src/sections/sliders/components/hero-slider/index.jsx
@@ -22,7 +22,7 @@ const HeroSlider = () => {
       {isSuccess &&
         data.map((item) => (
           <SwiperSlide key={item.id}>
-            <Card className={styles.card}>
+            <Card bsPrefix={styles.card}>
               <div className="row">
                 <div className="col-sm-12 col-md-6">
                   <CardHeader to={route.podcast} id={item.id} image={item.image} imageStyle={styles.image} />
