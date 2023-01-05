7c0ef522129f1b9a3d9dcf5c6332949b139545fc
Furkan <furi544@gmail.com>
2023-01-05 22:00:54 +0300

    video detail & other post

--- src/components/Detail/OtherPost.js
+++ src/components/Detail/OtherPost.js
@@ -1,20 +1,37 @@
-import BlogItem from 'components/Items/BlogItem'
-import { useGetBlogDataQuery } from 'store/api/admin-base'
 import { useParams } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { BlogCard, VideoCard } from 'sections'
+import { Spinner, Stack } from 'react-bootstrap'
+import styles from './index.module.scss'
 
-const OtherPost = () => {
+const OtherPost = (props) => {
+  const { data, loader, error, type } = props
   const { id } = useParams()
-  const { data, isSuccess } = useGetBlogDataQuery()
   const filteredData = data?.filter((f) => f.id !== Number(id))
+
+  if (loader) return <Spinner />
+  if (error) return <p>Error...</p>
 
   return (
     <div className="blog mt-5">
       <div className="titleDiv">
         <h3>OTHER POSTS</h3>
       </div>
-      {isSuccess && filteredData.map((item) => <BlogItem data={item} key={item.id} />)}
+      <Stack gap={4} direction="vertical">
+        {type === 'video' &&
+          !loader &&
+          filteredData.map((item) => <VideoCard key={item.id} data={item} className={styles.card} />)}
+        {type === 'blog' && !loader && filteredData.map((item) => <BlogCard key={item.id} data={item} />)}
+      </Stack>
     </div>
   )
+}
+
+OtherPost.propTypes = {
+  data: PropTypes.any,
+  loader: PropTypes.bool,
+  error: PropTypes.bool,
+  type: PropTypes.string,
 }
 
 export default OtherPost
