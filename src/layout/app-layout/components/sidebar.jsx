8fcafbbf28b777cb9a2ef9bb1d7056e885c8cb13
Furkan <furi544@gmail.com>
2023-01-06 23:59:19 +0300

    sidebar ui fixes

--- src/layout/app-layout/components/sidebar.jsx
+++ src/layout/app-layout/components/sidebar.jsx
@@ -1,20 +1,18 @@
 import Player from '../../../sections/player-desktop'
-import AdsBanner from '../../../sections/banner'
 import styles from '../style.module.scss'
 import { useSelector } from 'react-redux'
+import { Banner } from 'sections'
 
-function Sidebar() {
+export default function Sidebar() {
   const { current } = useSelector((state) => state.player)
 
   return (
     <div className={styles.sidebar}>
       <Player />
-      <div className={styles.sidebar__description}>
-        {' '}
-        <p>{current?.description}</p>
+      <div className={styles.description}>
+        <p className={styles.description__item}>{current?.description}</p>
       </div>
-      <AdsBanner />
+      <Banner />
     </div>
   )
 }
-export default Sidebar
--- src/sections/banner/index.jsx
+++ src/sections/banner/index.jsx
@@ -1,11 +1,10 @@
-import clsx from 'clsx'
 import playImage from '../../assets/img/banner.png'
 import styles from './index.module.scss'
 
 export default function Banner() {
   return (
-    <div className={clsx('d-none d-md-block w-100', styles.banner)}>
-      <img src={playImage} className="w-100 h-100" />
+    <div className={styles.banner}>
+      <img src={playImage} className={styles.banner__img} />
     </div>
   )
 }
