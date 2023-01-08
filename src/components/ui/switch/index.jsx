7d7f514b548b92e8f9951b706091c20067a2e5f4
Furkan <furi544@gmail.com>
2023-01-08 14:09:46 +0300

    switch ui

--- src/components/ui/switch/index.jsx
+++ src/components/ui/switch/index.jsx
@@ -64,7 +64,7 @@ export default function Switch({
 
 Switch.defaultProps = {
   smallSwitch: false,
-  optionLabels: ['Radio', 'Podcast'],
+  optionLabels: ['RADIO', 'PODCAST'],
 }
 
 Switch.propTypes = {
